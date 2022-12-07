5591ee01a11b8583b75384f8a89e424a051fc3c0
Anastasiya <Anastasiya@DELL-3779>
2022-12-07 21:22:35 +0200

    first commit

--- src/main/java/telran/java2022/KarafkaConsumerApplication.java
+++ src/main/java/telran/java2022/KarafkaConsumerApplication.java
@@ -4,14 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class KarafkaConsumerApplication {
 
 	
 	public static void main(String[] args) {
 		SpringApplication.run(KarafkaConsumerApplication.class, args);
-
 	}
-	
 
 }
